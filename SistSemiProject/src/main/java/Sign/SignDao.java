ddf6efb4c96a0e27ea63cbfccce4f431a4eab556
kjy97 <kjy2000@naver.com>
2021-11-02 13:49:59 +0900

    signup

--- SistSemiProject/src/main/java/Sign/SignDao.java
+++ SistSemiProject/src/main/java/Sign/SignDao.java
@@ -17,12 +17,17 @@ public class SignDao {
 		Connection conn=db.getConnection();
 		PreparedStatement pstmt=null;
 		
-		String sql="insert into member () values()";
+		String sql="insert into member(id,pass1,pass2,birth,hp,email) values(?,?,?,?,?,?)";
 		
 		try {
 			pstmt=conn.prepareStatement(sql);
 			
 			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPass1());
+			pstmt.setString(3, dto.getPass2());
+			pstmt.setString(4, dto.getBirth());
+			pstmt.setString(5, dto.getHp());
+			pstmt.setString(6, dto.getEmail());
 			
 			pstmt.execute();
 		} catch (SQLException e) {
