712d15b28926274b2ab195cd0e49a89ea1cae00a
msrim <msrim@192.168.0.49>
2021-11-08 16:31:57 +0900

    ReverseDao 수정

--- SistSemiProject/src/main/java/reserve/ReserveDao.java
+++ SistSemiProject/src/main/java/reserve/ReserveDao.java
@@ -104,11 +104,38 @@ public class ReserveDao {
 	}
 	
 	public void deleteReserve(String reserve_num) {
+		ReserveDto dto = new ReserveDto();
 		Connection conn = db.getConnection();
 		Statement stmt = null;
 		try {
 			stmt = conn.createStatement();
+			dto = this.getData(reserve_num);
 			stmt.execute("DELETE FROM reserve WHERE reserve_num='" + reserve_num + "'");
+			falseSeatCheck(dto.getSeat_id(), dto.getTheater_num());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			db.dbClose(stmt, conn);
+		}
+	}
+	
+	public void falseSeatCheck(String seat_id, String theater_num) {
+		String sql = "UPDATE seat SET chk_seat=true WHERE (theater_num='" + theater_num + "') AND (";
+		if (seat_id.contains(":")) {
+			String[] seat_ids = seat_id.split(":");
+			for (String i : seat_ids) {
+				sql += "seat_id='" + i + "' OR ";
+			}
+			sql = sql.substring(0, sql.length() - 4) + ")";
+		} else {
+			sql += "seat_id='" + seat_id + "')";
+		}
+		
+		Connection conn = db.getConnection();
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+			stmt.execute(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
